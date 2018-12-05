38692a6aaae42226b1ab54573768a3976e5788ac
KeesKezer <Kees_keijser@hotmail.com>
2018-12-05 16:36:02 +0100

    added welcome page

--- test.php
+++ test.php
@@ -1,12 +1,4 @@
 <?php
-  $names = array("John", "Eva", "Marcel", "Ronald");
-  end($names);
 
-  while(!is_null($key = key($names)))
-  {
-  	$val = current($names);
-  	echo "$key => $val\n";
-  	prev($names);
-    echo $key ($names);
-  }
+phpinfo();
 ?>
